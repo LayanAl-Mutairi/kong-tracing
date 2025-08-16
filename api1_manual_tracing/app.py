39fe54c2be6d76a38b09cffdb0ee5f86dbb53c49
LayanAl-Mutairi <layanal-mutairi@outlook.sa>
2025-08-17 02:15:06 +0300

    Update app.py

--- api1_manual_tracing/app.py
+++ api1_manual_tracing/app.py
@@ -7,7 +7,6 @@ from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExport
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 import random
 
-# Resource باسم الخدمة
 resource = Resource(attributes={"service.name": "api1-service"})
 trace.set_tracer_provider(TracerProvider(resource=resource))
 tracer = trace.get_tracer(__name__)
@@ -15,7 +14,7 @@ exporter = OTLPSpanExporter(endpoint="http://jaeger:4318/v1/traces")
 trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(exporter))
 
 app = Flask(__name__)
-FlaskInstrumentor().instrument_app(app)  # instrumentation حقيقية للـ Flask
+FlaskInstrumentor().instrument_app(app)  
 
 @app.route("/order")
 def order():
@@ -23,21 +22,18 @@ def order():
     user_id = f"user-{random.randint(1,10)}"
     amount = random.randint(50,500)
 
-    # Span رئيسي للعملية
+    
     with tracer.start_as_current_span("manual_order_process") as span_main:
         span_main.set_attribute("order.id", order_id)
         span_main.set_attribute("user.id", user_id)
         span_main.set_attribute("order.amount", amount)
 
-        # Span فرعي: التحقق من المخزون
         with tracer.start_as_current_span("check_inventory") as span_inv:
             span_inv.set_attribute("items.available", True)
 
-        # Span فرعي: احتساب الضريبة
         with tracer.start_as_current_span("calculate_tax") as span_tax:
             span_tax.set_attribute("tax.amount", round(amount*0.15,2))
 
-        # Span فرعي: إنشاء الفاتورة
         with tracer.start_as_current_span("generate_invoice") as span_invoice:
             span_invoice.set_attribute("invoice.id", f"inv-{random.randint(1000,9999)}")
 
